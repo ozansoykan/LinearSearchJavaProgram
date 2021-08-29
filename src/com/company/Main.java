c00ecf2dcf8049acc7975a80bb7d22d770ecb19d
ozansoykan <ozansoykan0@gmail.com>
2021-08-30 01:50:23 +0300

    Edit linear search algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int number = 4;
+	    int[] numbers = {1,2,3,4,5,6,7,8};
+	    int size = numbers.length;
+        System.out.println("index of number : "+linearSearch(number,size,numbers));
+    }
+    static int linearSearch(int number , int size , int[] numbers){
+        for (int i=0;i<size;i++){
+            if (numbers[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
